da08e86cad894bdf7b4e5bc7913274733f904e68
Thapelo A Motsumi <Thapelo.umuziphotoclub@gmail.com>
2023-04-23 22:54:42 +0200

    first commit

---
+++ IWA_4.3:Challenge 1/script.js
@@ -0,0 +1,34 @@
+const date = 2050;/* they was : not = hence code is not working*/
+const status= 'student'; /* they was : not = hence code is not working, also no semilon*/
+const total = 0;  /* no semilon*/
+
+if ( date = 2050) {
+   console.log( "January" , "New Year's Day" ) /* They did not close the brackets, semilon*/
+   console.log("March,Human Rights Day")
+   console.log("date, Family Day")
+   console.log("date, freedom Day")
+let conunt = conunt + 4
+
+if ( status = "student") {
+  console.log( 'June', 'Youth Day')
+  let count = count + 1 
+}
+  
+console.log ( "August", "Women's Day")
+console.log ("September", "Heritage Day")
+date = 'December'
+console.log (date, "Day of Reconciliation")
+let count = count + 3
+
+if (status ="parent"){
+  console.log(date, "Christmas Day")
+  let count = count + 1
+}
+
+console.log( date, "Day of Goodwill")
+let count = count + 1
+
+}
+console.log( Your status is: ', status )
+console.log(The year is: ', date)
+console.log(The total holidays is:', count)
